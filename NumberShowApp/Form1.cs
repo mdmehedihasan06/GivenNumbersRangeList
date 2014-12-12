1e46bd0a0c6048a8a0065ddeb38a481572bf994a
Md-Mehedi-Hasan <smmehedi06@gmail.com>
2014-12-12 23:03:02 +0600

    Number Show Files Added

---
+++ NumberShowApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberShowApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number = Convert.ToDouble(textBox1.Text);
+            listBox1.Items.Clear();
+            for (double i = 1; i <= number; i++)
+            {
+                string result = Convert.ToString(i);
+                listBox1.Items.Add(result);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
